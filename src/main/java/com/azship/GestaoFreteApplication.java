74ecd3f1105eb720426137140cf67e7cd7aa4750
Henrique Rodrigues <hyck.rocha@gmail.com>
2022-04-21 08:45:35 -0300

    Projeto Criado

---
+++ src/main/java/com/azship/GestaoFreteApplication.java
@@ -0,0 +1,13 @@
+package com.azship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFreteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azship/GestaoFreteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
